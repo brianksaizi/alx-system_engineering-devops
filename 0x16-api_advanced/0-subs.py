0ffce80fe419a88b96f576e6047eae2f80dc92e4
brianksaizi <saizibk@gmail.com>
2024-05-07 22:17:34 +0000

    v7

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,12 @@
 #!/usr/bin/python3
-"""How many Subs?"""
+"""
+0-main
+"""
+import sys
 
-import requests
-
-def number_of_subscribers(subreddit):
-    """Queries the Reddit API and returns the number of subscribers
-    to subreddit"""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
+if __name__ == '__main__':
+    number_of_subscribers = __import__('0-subs').number_of_subscribers
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
     else:
-        return 0
+        print("{:d}".format(number_of_subscribers(sys.argv[1])))
